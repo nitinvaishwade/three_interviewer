ead920959757e499172fced59ee2a9c3a307db7b
Nitin Vaishwade <vaishwadenitin@gmail.com>
2016-04-13 00:37:26 +0530

    ui-design
    UI_design

--- interviewer_app/admin.py
+++ interviewer_app/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from interviewer_app.models import Candidate, Score
+
+class CandidateAdmin(admin.ModelAdmin):
+ list_display = ( 'first_name', 'last_name', 'email' )
+ search_fields = [ 'first_name' ]
+ 
+ 
+class ScoreAdmin(admin.ModelAdmin):
+ list_display = ( 'interviwer',  'candidate', 'score' )
+ 
+
+admin.site.register(Candidate,CandidateAdmin)
+admin.site.register(Score,ScoreAdmin)
